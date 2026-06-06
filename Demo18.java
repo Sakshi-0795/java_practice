338294e86d562c370a2bb0d3bf677b1af249fb81
Sakshi more <moresakshi796@gmail.com>
2026-06-06 21:40:15 +0530

    Add files via upload

---
+++ Demo18.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+
+class Demo18{
+    public static void main(String[]args){
+
+        TreeSet<Integer> set = new TreeSet<>();
+        
+        set.add(50);
+        set.add(10);
+        set.add(30);
+
+        System.out.println(set);
+        }
+}
