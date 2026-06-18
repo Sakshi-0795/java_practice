57e100ecddbb75ff3281e2c9f08352f5890ee0ba
Sakshi more <moresakshi796@gmail.com>
2026-06-18 20:06:58 +0530

    Add files via upload

---
+++ Demo20.java
@@ -0,0 +1,12 @@
+import java.util.TreeMap;
+
+class Demo20{
+    public static void main(String[]args){
+        TreeMap<Integer,String> map = new TreeMap<>();
+        map.put(1,"Java");
+        map.put(2,"Html");
+        map.put(3,"Python");
+
+        System.out.println(map);
+    }
+}
