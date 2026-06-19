d71094f3f5115b646fa3b73a354c9f25daedd563
Sakshi more <moresakshi796@gmail.com>
2026-06-19 20:12:02 +0530

    Add files via upload

---
+++ Demo19.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+class Demo19{
+    public static void main(String[]args){
+        HashMap<Integer , String> map = new HashMap<>();
+        map.put(1,"Sakshi");
+        map.put(2,"Sagar");
+
+        System.out.println(map);
+    }
+}
