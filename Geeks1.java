1deed24451a8d99b45d512a75524aed3d8fe82f4
Sakshi more <moresakshi796@gmail.com>
2026-06-21 18:41:20 +0530

    Add files via upload

---
+++ Geeks1.java
@@ -0,0 +1,9 @@
+class Geeks1{
+    public static void main(String[]args){
+        String[] names = {"Ram","Krishna"};
+
+        for(String name : names){
+            System.out.println("Name: " + name);
+        }
+    }
+}
